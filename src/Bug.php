8640ce04dab8d05591656a97eed3ae9f6d8109a3
Chanfi Mari Yram <152385957+JusDeFruits1@users.noreply.github.com>
2025-05-13 16:28:59 +0200

    Entity Repositories

--- src/Bug.php
+++ src/Bug.php
@@ -5,7 +5,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use DateTime as DateTime;
 use Doctrine\ORM\Mapping as ORM;
- #[ORM\Entity]
+
+ #[ORM\Entity(repositoryClass: BugRepository::class)]
  #[ORM\Table(name: 'bugs')]
  class Bug
  {
